74392dc2b0164d7d110f66645fe069c4467569be
Ezreal <Ezreal_9527@163.com>
2018-01-25 18:21:57 +0800

    readme.txt commit

---
+++ test.c
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author:printf("\033[1;32;40m string \033[0m") 
+	> Mail: wangrq@farsight.com.cn
+	> Created Time: 2018年01月25日 星期四 13时33分19秒
+ ************************************************************************/
+ 
+#include<stdio.h>
+int main(int argc, char *argv[])
+{
+    printf("hello brother\n");
+}
